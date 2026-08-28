dfb7d2fdbd7029233a67e4e60e8b182da32c7dd1
sflande10 <sflande10@gmail.com>
2026-08-28 19:06:00 -0400

    A change to the first answer.

--- test_question_1.py
+++ test_question_1.py
@@ -5,3 +5,8 @@ course_of_study = "Computer Science"
 # Displays variables using string formatting
 print(f"My name is {name}, I am {age} years old and I study {course_of_study}.")
 print("More changes is all i am asking for. thank you")
+name = "Barry"
+age = 26
+course_of_study = "Computer Science"
+sport = "basketball"
+print(f"My name is {name}, I am {age} years old. I study {course_of_study} and play {sport}")
