d91615a8e8577b9bf66b6a20d9627de320e00a29
Tope Oyebamiji <it@isaluhospitals.com>
2026-08-28 23:37:34 +0100

    new changes tope made to sf project

--- test_question_1.py
+++ test_question_1.py
@@ -4,3 +4,4 @@ age = 24
 course_of_study = "Computer Science"
 # Displays variables using string formatting
 print(f"My name is {name}, I am {age} years old and I study {course_of_study}.")
+print("More changes is all i am asking for. thank you")
